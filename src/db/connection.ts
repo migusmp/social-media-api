40a7d4b93c9ec120c2f3aad4a3785058f926f1dc
migusmp <mmp2004sp@gmail.com>
2024-08-08 20:58:56 +0200

    database connection function created

---
+++ src/db/connection.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const db_url = process.env.DATABASE_URL;
+
+const connection = async () => {
+    try {
+        await mongoose.connect(`mongodb://${db_url}`)
+        console.log("CONEXIÓN A LA BASE DE DATOS EXITOSA!");
+    } catch(e) {
+        console.error("Error al establecer conexión con la BBDD:", e);
+    }
+}
+
+export default connection;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 
+// Importamos la función encargada de conectarnos con la BBDD MongoDB
+import connection from './db/connection';
+
 // Importamos el router de la API
 import MainRouter from './routes/index';
+
+connection(); // Establecemos conexión con la BBDD
 
 const app = express() // Inicializamos la app
 
